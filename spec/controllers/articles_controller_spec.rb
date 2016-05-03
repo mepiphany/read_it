805cd779c5b7ce7a02eb3216762a6f1a809c82ed
Michael Lee <mike.epiphany@gmail.com>
2016-05-03 16:31:18 -0700

    add rspec show, edit, update, destory

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-  describe "Get #Index" do
+
+  describe "#Index" do
     it "renders the index template" do
       get :index
       expect(response).to be_success
@@ -26,6 +27,7 @@ RSpec.describe ArticlesController, type: :controller do
 
       get :new
       expect(assigns(:article)).to be_a_new(Article)
+      binding.pry
     end
   end
 
@@ -75,8 +77,98 @@ RSpec.describe ArticlesController, type: :controller do
         invalid_request
         expect(flash[:alert]).to be
       end
+    end
 
 
+  end
+
+  describe "#show" do
+    it "find the object by its id and sets to @article variable" do
+      article = Article.create(title: "show_test", body: "body_test")
+      get :show, id: article.id
+      expect(assigns(:article)).to eq(article)
+
+    end
+    it "renders the show page" do
+      article = Article.create(title: "show_test", body: "body_test")
+      get :show, id: article.id
+      expect(response).to render_template(:show)
+    end
+    it "raises an error if the id passed doesn't match the record in the DB" do
+      article = Article.create(title: "show_test", body: "body_test")
+      expect { get :show, id: 23423 }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe "#edit" do
+    it "renders edit page" do
+      article = Article.create(title: "title", body: "body")
+      get :edit, id: article
+      expect(response).to render_template(:edit)
+    end
+    it "finds the article by the id and set it to @article" do
+      article = Article.create(title: "title", body: "body")
+      get :edit, id: article
+      expect(assigns(:article)).to eq(article)
+    end
+  end
+
+  describe "#update" do
+    context "with valid attributes" do
+      it "updates with new information" do
+        article = Article.create(title: "update_test", body: "update_body")
+        patch :update, id: article.id, article: {title: "updated!"}
+        expect(article.reload.title).to eq("updated!")
+    end
+      it "redirect_to show page" do
+        article = Article.create(title: "update_test", body: "update_body")
+        patch :update, id: article.id, article: {title: "updated!"}
+        article = article.reload
+        expect(response).to redirect_to(article_path(article))
+    end
+      it "display flash notice" do
+        article = Article.create(title: "update_test", body: "update_body")
+        patch :update, id: article.id, article: {title: "updated!"}
+        expect(flash[:notice]).to be
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not update information" do
+        article = Article.create(title: "update_test", body: "update_body")
+        patch :update, id: article.id, article: {title: "asdfsadfsadfsdafdsafsdafsdafsdaf"}
+        expect(article.reload.title).to_not eq("asdfsadfsadfsdafdsafsdafsdafsdaf")
+      end
+      it "renders edit page" do
+        article = Article.create(title: "update_test", body: "update_body")
+        patch :update, id: article.id, article: {title: "asdfsadfsadfsdafdsafsdafsdafsdaf"}
+        expect(response).to render_template(:edit)
+      end
+      it "display flash notice" do
+        article = Article.create(title: "update_test", body: "update_body")
+        patch :update, id: article.id, article: {title: "asdfsadfsadfsdafdsafsdafsdafsdaf"}
+        expect(flash[:alert]).to be
+      end
+    end
+  end
+
+  describe "#destroy" do
+    it "record destroyed from the database" do
+      article = Article.create(title: "delete_test", body: "delete_test")
+      article_count_before = Article.count
+      delete :destroy, id: article
+      article_count_after = Article.count
+      expect(article_count_after - article_count_before).to eq(-1)
+    end
+    it "redirect to index page" do
+      article = Article.create(title: "delete_test", body: "delete_test")
+      delete :destroy, id: article
+      expect(response).to redirect_to(articles_path)
+    end
+    it "display flash notice" do
+      article = Article.create(title: "delete_test", body: "delete_test")
+      delete :destroy, id: article
+      expect(flash[:notice]).to be
     end
   end
 end
